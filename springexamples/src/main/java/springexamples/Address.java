f646ab564fd4f0307fb13eb296abd3858f02bf47
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2022-03-01 17:43:21 +0530

    Add files via upload
    Constructor arg code is added

--- springexamples/src/main/java/springexamples/Address.java
+++ springexamples/src/main/java/springexamples/Address.java
@@ -3,26 +3,43 @@ package springexamples;
 
 public class Address{
 	public int dno;
-	String street;
-	String city;
+	public Address() {
+		
+	}
 	public int getDno() {
 		return dno;
 	}
+
 	public void setDno(int dno) {
 		this.dno = dno;
 	}
+
 	public String getStreet() {
 		return street;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
+	String street;
+	String city;
+	
+	public Address(int dno, String street, String city) {
+		super();
+		this.dno = dno;
+		this.street = street;
+		this.city = city;
+	}
+	
 	@Override
 	public String toString() {
 		return "Address [dno=" + dno + ", street=" + street + ", city=" + city + "]";
